cef595d4e45ec0df2ec0c0452ed865d1b125f3f4
Jin Sung Kim <mac@Jins-MacBook-Air.local>
2021-08-08 17:55:19 +0700

    sqlite삭제

--- apps/member/urls.py
+++ apps/member/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from .views import *
 
+app_name = "member"
+
 urlpatterns = [
     path('<int:pk>', MemberInfo.as_view(), name='member_info'),
-
 ]
