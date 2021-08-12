ea59e99eb70c5b4257e0b57421e8a040c191351f
커뮤니티 <community@macs-MacBook-Pro.local>
2021-08-12 21:29:07 +0700

    공고 수정기능 업데이트

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -8,4 +8,5 @@ app_name = "api"
 urlpatterns = [
     path('notice_list/<str:team_pk>', notice_list, name="notice_list"),
     path('notice_detail/<str:notice_pk>', notice_detail, name="notice_detail"),
+    path('notice_update/<str:notice_pk>', notice_update, name="notice_update"),
 ]
